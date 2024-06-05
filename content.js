a9c218aacfb8df0655607627657e0478ff495d7e
farque65 <fahim.farque@gmail.com>
2024-06-05 15:54:57 -0400

    initial code

---
+++ content.js
@@ -0,0 +1,44 @@
+// Define the selectors for different websites
+const selectors = {
+    'youtube.com': '.ytp-skip-ad-button',
+    // Add more websites and their skip button selectors here
+    'example.com': '.example-skip-ad-button'
+  };
+  
+  function getSelectorForCurrentSite() {
+    const hostname = window.location.hostname;
+    for (const site in selectors) {
+      if (hostname.includes(site)) {
+        return selectors[site];
+      }
+    }
+    return null;
+  }
+  
+  function skipAds(selector) {
+    const skipButton = document.querySelector(selector);
+    if (skipButton) {
+      skipButton.click();
+    }
+  }
+  
+  const selector = getSelectorForCurrentSite();
+  
+  if (selector) {
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        if (mutation.addedNodes.length > 0) {
+          skipAds(selector);
+        }
+      }
+    });
+  
+    observer.observe(document, {
+      childList: true,
+      subtree: true
+    });
+  
+    // Initial check in case the ad is already present
+    skipAds(selector);
+  }
+  
